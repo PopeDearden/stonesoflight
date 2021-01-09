25d0864ecbc873bd5d3748b2f8e482338b2b4360
taylor dearden <taylordearden@gmail.com>
2021-01-09 16:58:46 -0700

    updated links on cards

--- src/components/sections/Solution.jsx
+++ src/components/sections/Solution.jsx
@@ -20,23 +20,27 @@ function Solution() {
 
       <div className="Row">
 
-     
+      <a href="https://checkout.square.site/merchant/WFJDHKBHCFDKF/checkout/3NBHFIZQ4XMHWIJV65M25T54" target="_blank" rel="noreferrer">
         <div className="Card">
           <h2>$95</h2>
           <p>Pays for one child's education for an entire year!</p>
           <img src="https://cdn.shopify.com/s/files/1/0040/3786/9638/files/NXL_7927.jpg?v=1592607859" alt="Guatemalan Girl Studying"></img>
         </div>
-   
+   </a>
+   <a href="https://checkout.square.site/merchant/WFJDHKBHCFDKF/checkout/3NBHFIZQ4XMHWIJV65M25T54" target="_blank" rel="noreferrer">
         <div className="Card">
           <h2>$85</h2>
           <p>Provides a smart phone or tablet for a child to access the education platform for 1 year!</p>
           <img src="https://cdn.shopify.com/s/files/1/0040/3786/9638/files/kids_looking_at_phone_1.jpg?v=1610224337" alt="Guatemalan Girl Studying"></img>
         </div>
+        </a>
+        <a href="https://checkout.square.site/merchant/WFJDHKBHCFDKF/checkout/3NBHFIZQ4XMHWIJV65M25T54" target="_blank" rel="noreferrer">
         <div className="Card">
           <h2>$95</h2>
           <p>Pays for one child's access to a certified teacher for one year!</p>
           <img src="https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Guatemalan Girl Studying"></img>
         </div>
+        </a>
       </div>
     </div>
     
