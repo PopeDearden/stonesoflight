7a6c1cb0b38438609f236dbc95dfd62c7f7211ea
taylor dearden <taylordearden@gmail.com>
2021-01-09 15:11:45 -0700

    updated animation again

--- src/components/sections/Solution.jsx
+++ src/components/sections/Solution.jsx
@@ -3,8 +3,9 @@ import Zoom from 'react-reveal/Zoom';
 
 function Solution() {
   return (
-        <Zoom cascade>
+  
     <div className="Solution">
+          <Zoom>
       <h1>Here's how you can make a difference!</h1>
       <a href="https://checkout.square.site/merchant/WFJDHKBHCFDKF/checkout/3NBHFIZQ4XMHWIJV65M25T54" target="_blank" rel="noreferrer">
 
@@ -15,15 +16,17 @@ function Solution() {
 </div>
 </a>
       <h2>$275 will provide a full education package to a child in need:</h2>
+      </Zoom>
 
       <div className="Row">
 
-  
+     
         <div className="Card">
           <h2>$95</h2>
           <p>Pays for one child's education for an entire year!</p>
           <img src="https://cdn.shopify.com/s/files/1/0040/3786/9638/files/NXL_7927.jpg?v=1592607859" alt="Guatemalan Girl Studying"></img>
         </div>
+   
         <div className="Card">
           <h2>$85</h2>
           <p>Provides a smart phone or tablet for a child to access the education platform for 1 year!</p>
@@ -36,7 +39,7 @@ function Solution() {
         </div>
       </div>
     </div>
-        </Zoom>
+    
   );
 }
 
