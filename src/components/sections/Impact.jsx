e195cb62e3a1b1bb3e5769dcac22d78b65190488
taylor dearden <taylordearden@gmail.com>
2021-01-10 14:50:55 -0700

    updated text for timeline

--- src/components/sections/Impact.jsx
+++ src/components/sections/Impact.jsx
@@ -15,6 +15,7 @@ function Impact() {
   <Zoom>
   <p>Stones of Light Education Foundation has invested <b>$250,000</b> for impoverished children to receive an education!</p>
    <p><b>Thousands of hours</b> on the ground helping to improve the quality and scope of Guatemalan Education for those living in Extreme Poverty.</p>
+  <p>Partnered with Colegio Del Futuro many years ago, bringing the (Guatemalan based - Ministry of Education Certified) online/live mentored education system to <b>thousands of students in Central America</b> and beyond.</p>
   <p><b>Created an alliance with Snow College</b>, the number one Junior college in the United States, with<b> a number of students graduated or currently working on their degrees!</b></p>
 <p><b>Opened a new school</b> in Chisec, Guatemala 2021.</p>
 <p>There are exciting new opportunities, in today’s global environment, to improve the lives of children who are living in extreme poverty through education.  <b>The future is filled with light and hope!</b></p>  
