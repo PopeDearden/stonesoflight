0ebf71b698996d7926b29ad001255ee251fcdf28
taylor dearden <taylordearden@gmail.com>
2021-10-06 19:52:41 -0600

    updated pricing

--- src/components/sections/Solution.jsx
+++ src/components/sections/Solution.jsx
@@ -15,21 +15,21 @@ function Solution() {
   </h2>
 </div>
 </a>
-      <h2>$275 will provide a full education package to a child in need:</h2>
+      <h2>$395 will provide a full education package to a child in need:</h2>
       </Zoom>
 
       <div className="Row">
 
       <a href="https://checkout.square.site/merchant/WFJDHKBHCFDKF/checkout/3NBHFIZQ4XMHWIJV65M25T54" target="_blank" rel="noreferrer">
         <div className="Card">
-          <h2>$95</h2>
+          <h2>$180</h2>
           <p>Pays for one child's education for an entire year!</p>
           <img src="https://cdn.shopify.com/s/files/1/0040/3786/9638/files/NXL_7927.jpg?v=1592607859" alt="Guatemalan Girl Studying"></img>
         </div>
    </a>
    <a href="https://checkout.square.site/merchant/WFJDHKBHCFDKF/checkout/3NBHFIZQ4XMHWIJV65M25T54" target="_blank" rel="noreferrer">
         <div className="Card">
-          <h2>$85</h2>
+          <h2>$90</h2>
           <p>Provides a smart phone or tablet for a child to access the education platform for 1 year!</p>
           <img src="https://cdn.shopify.com/s/files/1/0040/3786/9638/files/Guatemalan_student_with_tablet.jpg?v=1615942822" alt="Guatemalan Boy with Tablet"></img>
           {/* <img src="https://cdn.shopify.com/s/files/1/0040/3786/9638/files/kids_looking_at_phone_1.jpg?v=1610224337" alt="Guatemalan Girl Studying"></img> */}
@@ -37,7 +37,7 @@ function Solution() {
         </a>
         <a href="https://checkout.square.site/merchant/WFJDHKBHCFDKF/checkout/3NBHFIZQ4XMHWIJV65M25T54" target="_blank" rel="noreferrer">
         <div className="Card">
-          <h2>$95</h2>
+          <h2>$125</h2>
           <p>Pays for one child's access to a certified teacher for one year!</p>
           <img src="https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Guatemalan Girl Studying"></img>
         </div>
